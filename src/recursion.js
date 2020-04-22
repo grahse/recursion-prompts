c8458879e28e35bef794ce6670cc59fe6151a4f1
grahse <gyykim@gmail.com>
2020-04-21 21:08:01 -0700

    Implement reverse and palindrome functions

--- src/recursion.js
+++ src/recursion.js
@@ -136,10 +136,32 @@ var powerOfTwo = function(n) {
 
 // 9. Write a function that reverses a string.
 var reverse = function(string) {
+  var reversed = '';
+  var index = string.length - 1;
+  reversed += string[index];
+
+  if (index === 0) {
+    return reversed
+  } else {
+    var copy = string.slice(0, index);
+    reversed += reverse(copy);
+  }
+
+  return reversed
 };
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  var copy = string.split('').join('').toLowerCase();
+  var index = string.length - 1;
+
+  if (string.length === 1 || string.length === 0) {
+    return true
+  } else if (copy[index] !== copy[0]) {
+    return false
+  } else {
+    return palindrome(copy.slice(1, index))
+  }
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
